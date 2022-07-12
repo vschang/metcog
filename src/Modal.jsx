2e100402e503da51584f6e4e2b9f1e53c636dfe7
Valerie Chang <valeriechang0@gmail.com>
2022-07-12 17:14:15 +0200

    Revert "changed gif to mp4"
    This reverts commit 560c4cfcb0d14053301889779b91de053a1d5419.

--- src/Modal.jsx
+++ src/Modal.jsx
@@ -1,21 +1,16 @@
 import './Modal.css'
 
 const Modal = props => {
-
-  // function restartVideo(){
-  //   const video = document.querySelector('video');
-  //   video.pause();
-  //   video.currentTime = 0;
-  //   video.load();
-  // }
+  const img = document.querySelector('img')
+  setTimeout(() => {
+    img.src = `${img.src.replace(/\?.*$/,"")}?x=${Math.random()}`;
+  }, 0)
 
   return (
     <div className={`modal ${props.show ? 'show' : ''}`}>
       <div className='modal-content'>
         <div className='modal-body'>
-          {/* <img src={props.src} className='Adjust-exercise-gif' alt={props.alt} /> */}
-          < video id="Exercise-vid" className='Adjust-exercise-gif' width="750" height="500" autoPlay={true} muted playsInline src={props.src} alt={props.alt} type="video/mp4">
-          </video>
+          <img src={props.src} className='Adjust-exercise-gif' alt={props.alt} />
         </div>
           <button onClick={props.onClose} className='close'><i className="fa-solid fa-xmark" ></i></button>
       </div>
